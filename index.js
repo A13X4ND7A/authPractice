89ebff4d7407ba9cb89b71c51b5ef25b9f35345e
Alexandra Faller <alex@alexfaller.uk>
2021-08-25 13:01:43 +0100

    adding basic password hashing

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const User = require('./models/user');
 const bcrypt = require('bcrypt');
 
-const hashedPassword = async () => {
-	const salt = await bcrypt.genSalt(10);
-	console.log(salt);
-};
+mongoose
+	.connect('mongodb://localhost:27017/authDemo', {useNewUrlParser: true})
+	.then(() => {
+		console.log(`Connected to MongoDB`);
+	})
+	.catch((err) => {
+		console.log(`Error connecting to MongoDB: ${err}`);
+	});
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+	res.send('This is the home page');
+});
+
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+	const {password, username} = req.body;
+	const hash = await bcrypt.hash(password, 12);
+	const user = new User({
+		username,
+		hashedPassword: hash,
+	});
+	await user.save();
+	res.redirect('/');
+});
+
+app.get('/secret', (req, res) => {
+	res.send('THIS IS A SECRET AND YOU CANNOT SEE IT UNLESS YOU ARE LOGGED IN!!');
+});
+
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
+});
